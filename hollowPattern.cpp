4a4428e3cfe9e00a8360b567dba396bef222e4a7
Prtish Behera <pritishbehera125@gmail.com>
2026-04-13 21:26:51 +0530

    solution for hollow pattern star printing

---
+++ hollowPattern.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n+1;j++){
+            if(j>0&&j<n&&i>0&&i<n-1){
+                cout<<" ";
+                continue;
+            }
+            cout<<"#";
+        }
+        cout<<"\n";
+    }
+}
