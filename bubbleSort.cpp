df30b382841a3a41f324ec2778078b46637484c7
Prtish Behera <pritishbehera125@gmail.com>
2026-05-10 22:29:39 +0530

    Updated with some code break points

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -3,10 +3,16 @@ using namespace std;
 
 void bubbleSort(int arr[],int n){
     for(int i=0;i<n-1;i++){
+        int isSwap=false;
         for(int j=0;j<n-i-1;j++){
             if(arr[j]>arr[j+1]){
                 swap(arr[j],arr[j+1]);
+                isSwap=true;
             }
+        }
+        if(isSwap){
+            cout<<"Already sorted";
+            break;
         }
     }
     cout<<"here is the sorted array\n";
