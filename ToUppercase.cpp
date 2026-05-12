bc0196ff94749303648e7b544d1247caab2b4165
Prtish Behera <pritishbehera125@gmail.com>
2026-05-12 19:55:45 +0530

    Solution for converting lowercase to uppercase in the char array

---
+++ ToUppercase.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void toUppercase(char word[]){
+    for(int i=0;i<strlen(word);i++){
+        if(word[i]>='A'&&word[i]<='Z'){
+            continue;
+        }else{
+            word[i]=word[i]-'a'+'A';
+        }
+    }
+    cout<<"the uppercase will be "<<word;
+}
+
+int main(){
+    int s;
+    cout<<"enter the size of char array:";
+    cin>>s;
+    cin.ignore();
+    s++;//this is increased because to able to get extra space for "\0" , it ensures end of char array
+    char word[s];
+    cout<<"enter values to char array:";
+    cin.getline(word,s,'*');
+    toUppercase(word);
+    return 0;
+}
