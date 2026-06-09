7dbc4e99f852bd93b37c6a44551afb56beb7b0c0
Prtish Behera <pritishbehera125@gmail.com>
2026-06-09 20:38:36 +0530

    Solution for printing all subset of a string

---
+++ subset.cpp
@@ -0,0 +1,30 @@
+//dont think)
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsetPrint(string str,string subset){
+    //base
+    if(str.size()==0){
+        cout<<subset<<endl;
+        return;
+    }
+    //karma
+    char ch = str.at(0);
+    //recurse call
+    subsetPrint(str.substr(1,str.size()-1),subset+=ch);
+    subsetPrint(str.substr(1,str.size()-1),subset);
+}
+int main(){
+    string str ="abc";
+    string subset="";
+    subsetPrint(str,subset);
+    return 0;
+}
+// o/p:
+// abc
+// ab
+// ac
+// a
+// bc
+// b
+// c
