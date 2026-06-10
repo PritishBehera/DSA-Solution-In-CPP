bf2084203d0fc0724ea5379c99c45daf5e33099c
Prtish Behera <pritishbehera125@gmail.com>
2026-06-10 20:33:55 +0530

    Solution for printing all possible permutation of string's character

---
+++ permutationString.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void permutation(string str, string ans){
+    int n=str.size();
+    if(n==0){
+        cout<<ans<<"\n";
+        return;
+    }
+    for(int i=0;i<n;i++){
+        char ch=str[i];
+        string newStr=str.substr(0,i)+str.substr(i+1,n-i-1);
+        permutation(newStr,ans+ch);
+    }
+}
+int main(){
+    string str="abc";
+    string ans="";
+    permutation(str,ans);
+    return 0;
+}
