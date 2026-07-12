e562e1dc1ea19999686496f5ddd040a09d7855d2
Prtish Behera <pritishbehera125@gmail.com>
2026-07-12 23:46:21 +0530

    updated codes for performing end vector to be sorted

--- maxActivity.cpp
+++ maxActivity.cpp
@@ -1,23 +1,34 @@
 #include<bits/stdc++.h>
 using namespace std;
-int maxActivity(vector<int>start,vector<int>end){
+bool compare(pair<int,int>p1,pair<int,int>p2){
+    return p1.second < p2.second;
+}
+void sortingEnd(vector<pair<int,int>>&pairs){
+    sort(pairs.begin(),pairs.end(),compare);
+}
+int maxActivity(vector<int>&start,vector<int>&end){
     //end must be sorted
+    vector<pair<int,int>> pairs;
+    for(int i=0;i<start.size();i++){
+        pairs.push_back({start[i],end[i]});
+    }
+    sortingEnd(pairs);
     //select start[0]
     int count=1;
     cout<<"A0"<<"\n";
-    int currEndTime=end[0];
-    for(int i=1;i<start.size();i++){
-        if(start[i]>=currEndTime){
+    int currEndTime=pairs[0].second;
+    for(int i=1;i<pairs.size();i++){
+        if(pairs[i].first>=currEndTime){
             cout<<"A"<<i<<"\n";
             count++;
-            currEndTime=end[i];
+            currEndTime=pairs[i].second;
         }
     }
     return count;
 }
 int main(){
-    vector<int>start={1,3,0,5,8,5};
-    vector<int>end={2,4,6,7,9,9};
+    vector<int>start={1,8,5,0,3,5};
+    vector<int>end={2,9,7,6,4,9};
     cout<<maxActivity(start,end);
     return 0;
 }
