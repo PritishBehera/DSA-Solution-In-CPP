b5a8e3e135be5dc932153bf43f7fefc58b1223de
Prtish Behera <pritishbehera125@gmail.com>
2026-07-14 23:46:56 +0530

    solution for min. absolute difference for possible pairs between 2 given array

---
+++ minAbsoluteDiff.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    vector<int> a={4,1,8,7};
+    vector<int> b={2,3,6,5};
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+    int ans=0;
+    for(int i=0;i<a.size();i++){
+        ans+=abs(a[i]-b[i]);
+    }
+    cout<<ans;
+    return 0;
+}
