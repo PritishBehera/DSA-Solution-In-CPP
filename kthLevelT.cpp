b51341bf4f5c13f065418dcc9b3439a466f099df
Prtish Behera <pritishbehera125@gmail.com>
2026-07-26 00:08:14 +0530

    solution for finding nodes at level k of a Binary Tree

---
+++ kthLevelT.cpp
@@ -0,0 +1,97 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        this->data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+static int idx = -1;
+Node* binaryTrees(vector<int>& nodesArr){
+    idx++;
+    if(nodesArr[idx]== -1){
+        return NULL;
+    }
+    Node* node = new Node(nodesArr[idx]);
+    node->left = binaryTrees(nodesArr);
+    node->right = binaryTrees(nodesArr);
+    return node;
+}
+void preorder(Node* root){
+    if(root==NULL){
+        return ;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+    //same can be written for inorder and postorder
+    //these all are implementation of depth first search(dfs)
+}
+void levelOrder(Node* root){//traversal(BFS)
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> qu;
+    qu.push(root);
+    qu.push(NULL);
+    while(!qu.empty()){
+        Node* curr=qu.front();
+        qu.pop();
+        if(curr==NULL){
+            cout<<"\n";
+            if(qu.empty()){
+                break;
+            }
+            qu.push(NULL);
+        }
+        else{
+            cout<<curr->data<<" ";
+            if(curr->left!=NULL){
+                qu.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                qu.push(curr->right);
+            }
+        }
+    }
+}
+void kthLevel(Node*root,int k){
+    queue<pair<Node*,int>> qu;
+    int c=1;
+    qu.push({root,c});
+    while(!qu.empty()){
+        Node* curr= qu.front().first;
+        int co= qu.front().second;
+        qu.pop();
+        //logic
+        if(co == k){
+            cout<<curr->data<<" ";
+        }
+        if(curr->left!=NULL){
+            qu.push({curr->left,co+1});
+        }
+        if(curr->right!=NULL){
+            qu.push({curr->right,co+1});
+        }
+    }
+}
+int main(){
+    vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+    Node* root = binaryTrees(nodes);
+    levelOrder(root);
+    int k=2;//level to print
+    cout<<"nodes at level:"<<k<<" is \n";
+    kthLevel(root,k);
+    return 0;
+}
+// output:
+// 1 
+// 2 3 
+// 4 5 6 
+// nodes at level:2 is 
+// 2 3 
