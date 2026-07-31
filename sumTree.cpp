eead658b03feff58cf949929569266e16e894ca8
Prtish Behera <pritishbehera125@gmail.com>
2026-08-01 00:06:47 +0530

    optimised the codelines

--- sumTree.cpp
+++ sumTree.cpp
@@ -66,15 +66,10 @@ int sumTree(Node*root){
     }
     int leftS = sumTree(root->left);
     int rightS = sumTree(root->right);
-    if(leftS==0 && rightS==0){
-        int nValue = root->data;
-        root->data=leftS+rightS;
-        return nValue;
-    }
+    
     int nValue = root->data;
     root->data=leftS+rightS;
     return root->data+nValue;
-    
 }
 int main(){
     vector<int> nodes={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
