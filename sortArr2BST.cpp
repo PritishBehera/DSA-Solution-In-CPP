e39e5cd207be85c60b06ccc1c1f86988ab83ec20
Prtish Behera <pritishbehera125@gmail.com>
2026-08-11 22:55:59 +0530

    solution for building BST from a given sorted arr/vector of node distinct node value

---
+++ sortArr2BST.cpp
@@ -0,0 +1,94 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+Node* insert(int val,Node* root){
+    if(root==NULL){
+        root = new Node(val);
+        return root;
+    }
+
+    if(val < root->data){
+        root->left = insert(val,root->left);
+    }else if(val > root->data){
+        root->right = insert(val,root->right);
+    }
+    return root;
+}
+Node* buildBST(int arr[],int n){
+    Node* root = NULL;
+    for(int i=0;i<=n-1;i++){
+        root = insert(arr[i],root);
+    }
+    return root;
+}
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+void levelOrder(Node* root){//traversal(BFS)
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> qu;
+    qu.push(root);
+    qu.push(NULL);
+    while(!qu.empty()){
+        Node* curr=qu.front();
+        qu.pop();
+        if(curr==NULL){
+            cout<<"\n";
+            if(qu.empty()){
+                break;
+            }
+            qu.push(NULL);
+        }
+        else{
+            cout<<curr->data<<" ";
+            if(curr->left!=NULL){
+                qu.push(curr->left);
+            }
+            if(curr->right!=NULL){
+                qu.push(curr->right);
+            }
+        }
+    }
+}
+int inorderSuccessor(Node* root){
+    while(root->left!=NULL){
+        root=root->left;
+    }
+    int va = root->data;
+    delete root;
+    return va;
+}
+Node* sortedArr2BST(vector<int>&vec,int st,int en){
+    if(st>en){
+        return NULL;
+    }
+    int mid=(st+en)/2;
+    Node* root = new Node(vec[mid]);
+    root->left = sortedArr2BST(vec,st,mid-1);
+    root->right = sortedArr2BST(vec,mid+1,en);
+    return root;
+}
+int main(){
+    vector<int> vec={3,4,5,6,7,8,9};
+    int n=vec.size();
+    Node* root = sortedArr2BST(vec,0,n-1);
+    levelOrder(root);
+    return 0;
+}
